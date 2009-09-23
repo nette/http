1080d59648f6bd2892f1aef3837959d862f20f2f
Jakub Vrana <jakub@vrana.cz>
2009-09-23 13:47:27 +0200

    HttpResponse: setHeader() can remove header in PHP 5.3

--- src/Http/HttpResponse.php
+++ src/Http/HttpResponse.php
@@ -115,8 +115,11 @@ final class HttpResponse extends /*Nette\*/Object implements IHttpResponse
 			throw new /*\*/InvalidStateException("Cannot send header after HTTP headers have been sent" . ($file ? " (output started at $file:$line)." : "."));
 		}
 
-		/*if ($value === NULL) header_remove($name); else*/
-		header($name . ': ' . $value, TRUE, $this->code);
+		if ($value === NULL && function_exists('header_remove')) {
+			header_remove($name);
+		} else {
+			header($name . ': ' . $value, TRUE, $this->code);
+		}
 		return $this;
 	}
 
