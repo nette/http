a640f50795370b91465969fc218e37169bcf2e83
David Grudl <david@grudl.com>
2014-08-18 00:54:31 +0200

    added HttpExtension & SessionExtension

---
+++ src/Bridges/HttpDI/HttpExtension.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (http://nette.org)
+ * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+ */
+
+namespace Nette\Bridges\HttpDI;
+
+use Nette;
+
+
+/**
+ * HTTP extension for Nette DI.
+ *
+ * @author     David Grudl
+ */
+class HttpExtension extends Nette\DI\CompilerExtension
+{
+	public $defaults = array(
+		'proxy' => array(),
+		'headers' => array(
+			'X-Powered-By' => 'Nette Framework',
+			'Content-Type' => 'text/html; charset=utf-8',
+		),
+		'frames' => 'SAMEORIGIN', // X-Frame-Options
+	);
+
+
+	public function loadConfiguration()
+	{
+		$container = $this->getContainerBuilder();
+		$config = $this->validateConfig($this->defaults);
+
+		$container->addDefinition($this->prefix('requestFactory'))
+			->setClass('Nette\Http\RequestFactory')
+			->addSetup('setProxy', array($config['proxy']));
+
+		$container->addDefinition($this->prefix('request'))
+			->setClass('Nette\Http\Request')
+			->setFactory('@Nette\Http\RequestFactory::createHttpRequest');
+
+		$container->addDefinition($this->prefix('response'))
+			->setClass('Nette\Http\Response');
+
+		$container->addDefinition($this->prefix('context'))
+			->setClass('Nette\Http\Context');
+
+		if ($this->name === 'http') {
+			$container->addAlias('nette.httpRequestFactory', $this->prefix('requestFactory'));
+			$container->addAlias('nette.httpContext', $this->prefix('context'));
+			$container->addAlias('httpRequest', $this->prefix('request'));
+			$container->addAlias('httpResponse', $this->prefix('response'));
+		}
+	}
+
+
+	public function afterCompile(Nette\PhpGenerator\ClassType $class)
+	{
+		$initialize = $class->methods['initialize'];
+		$config = $this->getConfig();
+
+		if (isset($config['frames']) && $config['frames'] !== TRUE) {
+			$frames = $config['frames'];
+			if ($frames === FALSE) {
+				$frames = 'DENY';
+			} elseif (preg_match('#^https?:#', $frames)) {
+				$frames = "ALLOW-FROM $frames";
+			}
+			$initialize->addBody('header(?);', array("X-Frame-Options: $frames"));
+		}
+
+		foreach ($config['headers'] as $key => $value) {
+			if ($value != NULL) { // intentionally ==
+				$initialize->addBody('header(?);', array("$key: $value"));
+			}
+		}
+	}
+
+}
---
+++ tests/Http.DI/SessionExtension.autoStart.phpt
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Test: SessionExtension.
+ */
+
+use Nette\DI,
+	Nette\Bridges\HttpDI\HttpExtension,
+	Nette\Bridges\HttpDI\SessionExtension,
+	Tester\Assert;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+
+$compiler = new DI\Compiler;
+$compiler->addExtension('foo', new HttpExtension);
+$compiler->addExtension('session', new SessionExtension);
+
+$loader = new DI\Config\Loader;
+$config = $loader->load(Tester\FileMock::create('
+session:
+	autoStart: yes
+', 'neon'));
+
+eval($compiler->compile($config, 'Container1'));
+
+$container = new Container1;
+
+$session = $container->getService('session.session');
+Assert::type('Nette\Http\Session', $session);
+Assert::false($session->isStarted());
+
+// aliases
+Assert::same($session, $container->getService('session'));
+
+
+$container->initialize();
+Assert::true($session->isStarted());
