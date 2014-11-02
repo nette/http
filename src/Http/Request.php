c0f47421072febff853d770eba44a761eb193778
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-11-02 03:27:04 +0100

    Request: default method is GET

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -75,7 +75,7 @@ class Request extends Nette\Object implements IRequest
 		$this->files = (array) $files;
 		$this->cookies = (array) $cookies;
 		$this->headers = array_change_key_case((array) $headers, CASE_LOWER);
-		$this->method = $method;
+		$this->method = $method ?: 'GET';
 		$this->remoteAddress = $remoteAddress;
 		$this->remoteHost = $remoteHost;
 		$this->rawBodyCallback = $rawBodyCallback;
