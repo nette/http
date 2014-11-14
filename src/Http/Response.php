44d58c4c5c68e00a243db4ff143b19e3d565c9ae
David Grudl <david@grudl.com>
2014-11-14 09:08:53 +0100

    Response::redirect() link is printed only for https? protocols [Closes #24]

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -147,8 +147,10 @@ class Response extends Nette\Object implements IResponse
 	{
 		$this->setCode($code);
 		$this->setHeader('Location', $url);
-		$escapedUrl = htmlSpecialChars($url, ENT_IGNORE | ENT_QUOTES);
-		echo "<h1>Redirect</h1>\n\n<p><a href=\"$escapedUrl\">Please click here to continue</a>.</p>";
+		if (preg_match('#^https?:|^[a-z0-9+.-]++[^:]#i', $url)) {
+			$escapedUrl = htmlSpecialChars($url, ENT_IGNORE | ENT_QUOTES);
+			echo "<h1>Redirect</h1>\n\n<p><a href=\"$escapedUrl\">Please click here to continue</a>.</p>";
+		}
 	}
 
 
