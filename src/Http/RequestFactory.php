8fe3a85bdca0edd41a481d4acfecbcbda1a50f2b
David Grudl <david@grudl.com>
2016-07-21 14:06:15 +0200

    RequestFactory: improved checking of $_FILES structure

--- src/Http/RequestFactory.php
+++ src/Http/RequestFactory.php
@@ -137,7 +137,9 @@ class RequestFactory extends Nette\Object
 		$list = array();
 		if (!empty($_FILES)) {
 			foreach ($_FILES as $k => $v) {
-				if (!$this->binary && is_string($k) && (!preg_match($reChars, $k) || preg_last_error())) {
+				if (!is_array($v) || !isset($v['name'], $v['type'], $v['size'], $v['tmp_name'], $v['error'])
+					|| (!$this->binary && is_string($k) && (!preg_match($reChars, $k) || preg_last_error()))
+				) {
 					continue;
 				}
 				$v['@'] = & $files[$k];
