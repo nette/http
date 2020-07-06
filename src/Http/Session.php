0759b831f7453ff81cf3a21b92d7d44b6a7b9138
David Grudl <david@grudl.com>
2020-07-07 00:10:25 +0200

    Session: deprecated getCookieParameters() triggers E_USER_DEPRECATED

--- src/Http/Session.php
+++ src/Http/Session.php
@@ -469,6 +469,7 @@ class Session
 	/** @deprecated */
 	public function getCookieParameters(): array
 	{
+		trigger_error(__METHOD__ . '() is deprecated.', E_USER_DEPRECATED);
 		return session_get_cookie_params();
 	}
 
