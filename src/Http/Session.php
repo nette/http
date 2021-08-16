55cda54c0fff3d45d082386262c59aa6292b4796
dakur <dev@kurowsti.cz>
2021-08-16 22:12:40 +0200

    Session: fixed condition in destroy() (#204)

--- src/Http/Session.php
+++ src/Http/Session.php
@@ -175,7 +175,7 @@ class Session
 	 */
 	public function destroy(): void
 	{
-		if (!session_status() === PHP_SESSION_ACTIVE) {
+		if (session_status() !== PHP_SESSION_ACTIVE) {
 			throw new Nette\InvalidStateException('Session is not started.');
 		}
 
