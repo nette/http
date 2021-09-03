a6b17b2965ccbeeea7893aad3941d1c9050d2edb
David Grudl <david@grudl.com>
2021-09-03 16:31:04 +0200

    used nette/utils 3.2

--- src/Http/Session.php
+++ src/Http/Session.php
@@ -128,7 +128,7 @@ class Session
 		}
 
 		$this->initialize();
-		$this->onStart($this);
+		Nette\Utils\Arrays::invoke($this->onStart, $this);
 	}
 
 
@@ -353,7 +353,7 @@ class Session
 			return;
 		}
 
-		$this->onBeforeWrite($this);
+		Nette\Utils\Arrays::invoke($this->onBeforeWrite, $this);
 
 		$nf = &$_SESSION['__NF'];
 		foreach ($nf['META'] ?? [] as $name => $foo) {
