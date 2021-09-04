31d3dec30d0eb64a7b8ee3e835c62bae136e887b
David Grudl <david@grudl.com>
2021-09-04 13:05:37 +0200

    Session::getIterator() is deprecated

--- src/Http/Session.php
+++ src/Http/Session.php
@@ -330,11 +330,10 @@ class Session
 	}
 
 
-	/**
-	 * Iteration over all sections.
-	 */
+	/** @deprecated */
 	public function getIterator(): \Iterator
 	{
+		trigger_error(__METHOD__ . '() is deprecated', E_USER_DEPRECATED);
 		if ($this->exists() && !$this->started) {
 			$this->autoStart(false);
 		}
